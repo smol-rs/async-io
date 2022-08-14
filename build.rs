1ece1535f629103e353cac0998be71820564e26a
jtnunley <jtnunley01@gmail.com>
2022-08-13 17:37:02 -0700

    use autocfg

---
+++ build.rs
@@ -0,0 +1,18 @@
+const TEST_EXPR: &str = "{
+#[cfg(unix)]
+fn _test<I: std::os::unix::io::AsFd>(_: I) {}
+#[cfg(unix)]
+fn _test2(_: std::os::unix::io::OwnedFd) {}
+#[cfg(windows)]
+fn _test<I: std::os::windows::io::AsSocket>(_: I) {}
+#[cfg(windows)]
+fn _test2(_: std::os::windows::io::OwnedSocket) {}
+}";
+
+fn main() {
+    let cfg = autocfg::new();
+    
+    if cfg.probe_expression(TEST_EXPR) {
+        autocfg::emit("has_io_safety");
+    }
+}
