918fce49b4eabad4c201a1e20656e43dd3ea3fed
cjw <cjwcommuny@outlook.com>
2023-09-12 21:23:47 +0800

    fix: Windows

--- src/lib.rs
+++ src/lib.rs
@@ -760,7 +760,7 @@ impl<T: AsSocket> Async<T> {
         let registration = unsafe { Registration::new(borrowed) };
 
         Ok(Async {
-            source: SourceContainer(Reactor::get().insert_io(registration)?),
+            source: Reactor::get().insert_io(registration)?.into(),
             io: io,
         })
     }
