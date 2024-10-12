374ebbbdb4e3c0665a981b1dd8bdec81b96a5cc9
Roger Coll <rogercoll@protonmail.com>
2024-10-12 16:09:36 +0200

    fix: use pid for exit example

--- examples/kqueue-process.rs
+++ examples/kqueue-process.rs
@@ -14,24 +14,34 @@
     target_os = "dragonfly",
 ))]
 fn main() -> std::io::Result<()> {
-    use std::process::Command;
+    use std::{num::NonZeroI32, process::Command};
 
     use async_io::os::kqueue::{Exit, Filter};
     use futures_lite::future;
 
     future::block_on(async {
         // Spawn a process.
-        let process = Command::new("sleep")
+        let mut process = Command::new("sleep")
             .arg("3")
             .spawn()
             .expect("failed to spawn process");
 
         // Wrap the process in an `Async` object that waits for it to exit.
-        let process = Filter::new(Exit::new(process))?;
+        let process_handle = Filter::new(Exit::new(
+            NonZeroI32::new(process.id().try_into().expect("invalid process pid"))
+                .expect("non zero pid"),
+        ))?;
 
         // Wait for the process to exit.
-        process.ready().await?;
+        process_handle.ready().await?;
 
+        println!(
+            "Process exit code {:?}",
+            process
+                .try_wait()
+                .expect("error while waiting process")
+                .expect("process did not exit yet")
+        );
         Ok(())
     })
 }
